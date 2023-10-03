13bd046a5773bf80e59f14df1bb49eead7b9b9fc
Anubhav-Ghosh1 <iamanubhav11@gmail.com>
2023-10-03 18:05:56 +0530

    Added Code

---
+++ LeetCode/Fibonacci Number/fibo.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int fibo(int n)
+    {
+        if(n == 0)
+        {
+            return 0;
+        }
+        if(n == 1)
+        {
+            return 1;
+        }
+        return (fibo(n-1) + fibo(n-2));
+    }
+    int fib(int n) {
+      return fibo(n);  
+    }
+};
