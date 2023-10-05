b7e7780587c8745fd9d35f676d129c97e90e8d35
Rishabh-198 <shukla.rishabh198@gmail.com>
2023-10-05 23:40:32 +0530

    my commit for issue 174

---
+++ LeetCode/174-Container-With-Most-Water/Java/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxArea(int[] height) {
+         int maxarea = 0;
+        int left = 0; 
+        int right = height.length - 1;
+        while (left < right) {
+            int width = right - left;
+            maxarea = Math.max(maxarea, Math.min(height[left], height[right]) * width);
+            if (height[left] <= height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return maxarea;
+    }
+    }
